cab4b5040c90d4f34929b98b9bc487d0ddb9ee57
marcolinardi00 <marcolinardi00@gmail.com>
2026-08-15 12:36:32 +0700

    refactor: change frame image style

--- components/Frames/ImageFrame.tsx
+++ components/Frames/ImageFrame.tsx
@@ -31,28 +31,23 @@ export function ImageFrame({ src, alt, classname = "", frameClass = "aspect-vide
           className="object-cover z-0 brightness-90 contrast-110 saturate-75 transition-transform duration-500 ease-out group-hover:scale-110"
         />
 
-        <div
-          className="absolute inset-0 z-10 pointer-events-none opacity-[0.08]"
-          style={{
-            backgroundImage:
-              'repeating-linear-gradient(0deg, #fff 0px, transparent 1px, transparent 2px)',
-          }}
-        />
-
-        <div className="absolute z-20 inset-4 pointer-events-none border border-cmdlet/30 transition-all duration-500 ease-out group-hover:inset-6">
-          <div className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 text-border font-mono text-2xl leading-none">
-            ┌
-          </div>
-          <div className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 text-border font-mono text-2xl leading-none">
-            ┐
-          </div>
-          <div className="absolute bottom-0 left-0 -translate-x-1/2 translate-y-1/2 text-border font-mono text-2xl leading-none">
-            └
-          </div>
-          <div className="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2 text-border font-mono text-2xl leading-none">
-            ┘
-          </div>
+      <div className="absolute z-20 inset-4 pointer-events-none transition-all opacity-45 duration-500 ease-out group-hover:inset-6">
+        <div className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 text-cmdlet font-mono text-2xl leading-none drop-shadow-[2px_2px_3px_rgba(0,0,0,0.6)]">
+          ┌
         </div>
+
+        <div className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 text-cmdlet font-mono text-2xl leading-none drop-shadow-[2px_2px_3px_rgba(0,0,0,0.6)]">
+          ┐
+        </div>
+
+        <div className="absolute bottom-0 left-0 -translate-x-1/2 translate-y-1/2 text-cmdlet font-mono text-2xl leading-none drop-shadow-[2px_2px_3px_rgba(0,0,0,0.6)]">
+          └
+        </div>
+
+        <div className="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2 text-cmdlet font-mono text-2xl leading-none drop-shadow-[2px_2px_3px_rgba(0,0,0,0.6)]">
+          ┘
+        </div>
+      </div>
       </button>
 
       <ImageModal
